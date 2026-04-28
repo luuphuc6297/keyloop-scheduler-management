12e97e1d6ab12c36159fc5718de4290d12b4eb61
Luu <luuphuc6297@gmail.com>
2026-04-29 00:39:56 +0700

    chore: add ESLint and Prettier configs (Tasks 18-19)

---
+++ packages/api/.eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: false,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: './tsconfig.json', sourceType: 'module' },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  env: { node: true, jest: true },
+  ignorePatterns: ['dist', 'coverage', '.eslintrc.cjs', 'jest*.config.ts'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-extraneous-class': 'off',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/no-empty-object-type': 'off',
+    'prettier/prettier': 'error',
+  },
+  overrides: [
+    {
+      files: ['**/*.spec.ts', '**/*.int-spec.ts', '**/*.e2e-spec.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+      },
+    },
+  ],
+};
